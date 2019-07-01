f917a166bd8ff27b2b9547d558f2f6d7dfe5902d
127.0.0.1 <22117518+samsesh@users.noreply.github.com>
2019-07-01 16:54:57 +0430

    Create tor-for-telegram.sh

---
+++ tor-for-telegram.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+clear
+echo “install proxy for telegram in termux”
+sleep 4
+echo “add proxy socks5”
+sleep 5
+echo “address : 127.0.0.1”
+sleep 3 
+echo “port : 9050”
+sleep 5
+echo “then wait to installing... ”
+sleep 6
+clear
+echo “Installing ...”
+sleep 1
+clear
+echo “iNstalling ...”
+sleep 1
+clear 
+echo “inStalling ...”
+sleep 1
+clear
+echo “insTalling ...”
+sleep 1
+clear
+echo “instAlling ...”
+sleep 1
+clear
+echo “instaLling ...”
+sleep 1
+clear
+echo “instalLing ...”
+sleep 1
+clear
+echo “installIng ...”
+sleep 1
+clear
+echo “installiNg ...”
+sleep 1
+clear
+echo “installinG ...”
+sleep 1
+clear
+apt update
+apt upgrade -y
+apt install figlet 
+figlet -f banner wait
+sleep 3
+figlet -f digital start
+echo “installing....”
+apt install tor zsh wget git -y
+figlet wait 
+sleep 3
+sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo “type ‘zsh’ then press enter”
+chsh
+cd ~
+echo ‘figlet -f big WELCOME’ >> .zshrc
+echo ‘sleep 5’ >> .zshrc
+echo ‘figlet -f standard code by init__0’ >> .zshrc
+echo ‘sleep 4’ >> .zshrc
+echo ‘figlet -f digital wait to run’ >> .zshrc
+echo ‘sleep 5’ >> .zshrc
+echo ‘clear’ >> .zshrc
+echo ‘figlet -f standard Tor’ >> .zshrc
+echo ‘sleep 1’ >> .zshrc
+echo ‘clear’ >> .zshrc
+echo ‘figlet -f standard tOr’ >> .zshrc
+echo ‘sleep 1’ >> .zshrc
+echo ‘clear’ >> .zshrc
+echo ‘figlet -f standard toR’ >> .zshrc
+echo ‘sleep 1’ >> .zshrc
+echo ‘clear’ >> .zshrc
+echo ‘figlet -f big run TOR’ >> .zshrc
+echo ‘sleep 5’ >> .zshrc
+echo ‘tor --HTTPTunnelPort 8118’ >> .zshrc
+‎‏echo ‘tor’ >> .zshrc
+figlet -f big DONE
+echo “close termux and run again”
+sleep 3
+exit
