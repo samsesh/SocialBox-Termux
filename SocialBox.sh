8f86a4540775545462b08346df8a37d9ca93c239
127.0.0.1 <22117518+samsesh@users.noreply.github.com>
2021-07-04 13:32:32 +0430

    Update SocialBox.sh

--- SocialBox.sh
+++ SocialBox.sh
@@ -34,7 +34,7 @@ echo -e "		 ▀▀▀▀  ▀█▄▀▪·▀▀▀ ▀▀▀ ▀  ▀ .▀▀�
 #echo -e "	[+]              $red Coded By sam.sesh$nc         		   [+]"
 #echo -e "	[+] 		 $red samsesh.net $nc 		   [+]"
 #echo -e "	[+] 		 $red Greetz To All Pentesters$nc          		   [+]"
-#echo -e "	[+]$red Special Greetz To$nc :$green {thelinuxchoice , Ha3MrX, Tunisian Eagles, init__0}$nc [+]"
+#echo -e "	[+]$red Special Greetz To$nc :$green {thelinuxchoice , Ha3MrX, Tunisian Eagles, samsesh}$nc [+]"
 echo ""
 echo -e "$yellow Select From Menu : $nc"
 echo ""
