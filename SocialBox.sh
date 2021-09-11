a9daf34820a4e17abc9c852d7bb0daa9f4cbddc8
127.0.0.1 <admin@samsesh.net>
2021-09-11 08:54:57 +0430

    fix bug on Instagram brute force 2
    disabled tor for fix and check problem later ...

--- SocialBox.sh
+++ SocialBox.sh
@@ -40,7 +40,7 @@ echo -e "		$Cyan 1 : Brute Force Facebook Account$nc"
 echo -e "		$Cyan 2 : Brute Force Gmail Account$nc"
 echo -e "		$Cyan 3 : Brute Force Instagram Account 1$nc"
 echo -e "		$Cyan 4 : Brute Force Twitter Account$nc"
-echo -e "		$Cyan 5 : Brute Force Instagram Account 2$nc"
+echo -e "		$Cyan 5 : Brute Force Instagram Account 2 (tor disabled)$nc"
 echo -e "		$Cyan 99: Exit$nc"
 read -p "Choice >  " ch
 if [ $ch = 1 ]; then
@@ -147,13 +147,13 @@ elif [ $ch = 5 ]; then
 echo -e "			$Cyan Instagram Brute Force 2$nc"
 cd insta-bf/
 sleep 0.025
-        echo -e "	[+]$red Service Tor Stated $nc[+]"
-sleep 0.9
-service tor start
+        echo -e "	[+]$red Service Tor disabled $nc[+]"
+#sleep 0.9
+#service tor start
 python instabf.py
 echo -e "               [+]$yellow Brute Force Complete $nc[$green✓$nc] $nc[+]"
-service tor stop
-echo -e "		[+]$red Service Tor Stopped$nc [+]"
+# service tor stop
+#echo -e "		[+]$red Service Tor Stopped$nc [+]"
 echo -e "$red"
 read -p "Wanna Back To Main Menu [ Y / n ] : " check5
 echo -e "$nc"
