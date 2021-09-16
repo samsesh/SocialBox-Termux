720a25409eb1f926035161666754cb084b2a76a7
127.0.0.1 <admin@samsesh.net>
2021-09-17 01:50:53 +0430

    Update install-sb.sh

--- install-sb.sh
+++ install-sb.sh
@@ -6,7 +6,7 @@ chmod 777 -R .
 apt-get update -qq >> /dev/null
 #apt full-upgrade 
 #req
-apt install python2 python tor perl figlet runit -y >> /dev/null
+apt install python2 python tor perl figlet runit openssl -y >> /dev/null
 clear
 sleep 2
 figlet Installing ...
